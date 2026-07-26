47d8ae04f68a14a3ce1b29f58d5d023c99e936a5
Behzad Shahbazi Fard <157199912+BSHF-PER@users.noreply.github.com>
2026-07-26 19:52:54 +0330

    New Includes

---
+++ www/includes/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'my_portfolio');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('SITE_URL', 'http://localhost/');
+define('BASE_PATH', __DIR__ . '/..');
+
+date_default_timezone_set('Asia/Tehran');
+session_start();
